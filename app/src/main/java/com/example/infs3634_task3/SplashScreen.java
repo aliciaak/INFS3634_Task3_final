aa140a32fcb1195109239eba8c089f19cb4f631a
Alicia <aliciayakurniawan@gmail.com>
2019-11-04 15:02:22 +1100

    splash home screen

--- app/src/main/java/com/example/infs3634_task3/SplashScreen.java
+++ app/src/main/java/com/example/infs3634_task3/SplashScreen.java
@@ -4,13 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ProgressBar;
 
 import com.felipecsl.gifimageview.library.GifImageView;
 
 import org.apache.commons.io.IOUtils;
+import android.os.Handler;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,8 +23,8 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_home);
 
-        gif = (GifImageView)findViewById(R.id.gif);
-        progress_bar = (ProgressBar)findViewById(R.id.progress_bar);
+        gif = (GifImageView) findViewById(R.id.gif);
+        progress_bar = (ProgressBar) findViewById(R.id.progress_bar);
         progress_bar.setVisibility(progress_bar.VISIBLE);
 
         try {
@@ -33,18 +32,16 @@ public class SplashScreen extends AppCompatActivity {
             byte[] bytes = IOUtils.toByteArray(inputStream);
             gif.setBytes(bytes);
             gif.startAnimation();
+        } catch (IOException ex) {
         }
-        catch (IOException ex){
-        }
+
+        //wait 2 seconds and start MainActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SplashScreen.this.startActivity(new Intent(SplashScreen.this.MainActivity.class));
+                SplashScreen.this.finish();
+            }
+        }, 2000); //2000 = 2 seconds
     }
-
-    //wait 2 seconds and start MainActivity
-    new Handler().postDelayed(new Runnable(){
-        @Override
-                public void run(){
-            SplashScreen.this.startActivity(new Intent(SplashScreen.this.MainActivity.class));
-            SplashScreen.this.finish();
-        }
-    },2000); //2000 = 2 seconds
-
 }
